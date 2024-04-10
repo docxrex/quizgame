e05890e5afef6b7bbf2c191987f3bf016070f12d
docxrex <166623051+docxrex@users.noreply.github.com>
2024-04-10 22:03:03 +0800

    Add files via upload

---
+++ second.js
@@ -0,0 +1,9 @@
+const proceedBtn = document.querySelector('.proceed-button');
+const popupInfo = document.querySelector('.popup-info');
+
+proceedBtn.onclick = () => {
+    popupInfo.classList.add('active');
+}
+
+
+    
